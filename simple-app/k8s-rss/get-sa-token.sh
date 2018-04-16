297881ac2473edd93409f9a0048213b816336eba
junaid-ali <junaidali.yahya@gmail.com>
2018-04-16 08:51:55 +0500

    Added helper files
      - get-sa-token.sh
        Create required namespace, service account and role+binding in k8s cluster
      - cluster-role-and-binding.yml
        Yaml file containing the resource spec of service account's cluster role
        and its binding
    
    Signed-off-by: junaid-ali <junaidali.yahya@gmail.com>

---
+++ simple-app/k8s-rss/get-sa-token.sh
@@ -0,0 +1,60 @@
+#!/bin/sh -e
+
+usage() {
+  echo ""
+  echo "Export K8S cert and service account token"
+  echo "usage: $(basename "$0") -n <namespace> -a <service_account>"
+  echo "  -n|--namespace The namespace of the service account"
+  echo "  -a|--account   The service account to export"
+  echo ""
+}
+
+err() {
+  printf "Please input both namespace and service_account.\\n"
+}
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -n | --namespace ) shift
+      if [[ -z $1 ]];
+      then
+  	    NAMESPACE="cd"
+      else
+        NAMESPACE=$1
+      fi
+      ;;
+    -a | --account ) shift
+      if [[ -z $1 ]];
+      then
+        ACCOUNT_NAME="cd-agent"
+      else
+        ACCOUNT_NAME=$1
+      fi
+      ;;
+    * ) usage
+      exit 1
+  esac
+  shift
+done
+
+# get API endpoint of current cluster
+CURR_CXT=$(kubectl config current-context)
+CURR_CLUSTER=$(kubectl config view -o jsonpath={.contexts[?\(@.name==\"$CURR_CXT\"\)].context.cluster})
+API_ENDPOINT=$(kubectl config view -o jsonpath={.clusters[?\(@.name==\"$CURR_CLUSTER\"\)].cluster.server})
+printf "API endpoint: \\n%s\\n" "$API_ENDPOINT"
+
+kubectl get ns $NAMESPCE > /dev/null 2>&1
+[[ $? -ne 0 ]] && kubectl create ns cd
+
+
+kubectl get sa $ACCOUNT_NAME -n $NAMESPACE > /dev/null 2>&1
+[[ $? -ne 0 ]] && kubectl create sa $ACCOUNT_NAME -n $NAMESPACE
+
+kubectl apply -f ./cluster-role-and-binding.yml
+
+ACCOUNT_SECRET=$(kubectl get sa ${ACCOUNT_NAME} -n ${NAMESPACE} -o jsonpath="{.secrets[].name}")
+kubectl get secret ${ACCOUNT_SECRET} -n ${NAMESPACE} -o go-template='{{index .data "ca.crt"}}' | base64 --decode > ca.crt
+
+SERVICE_ACCOUNT_TOKEN_IN_K8S=$(kubectl get secret ${ACCOUNT_SECRET} -n ${NAMESPACE} -o jsonpath="{.data['token']}" | base64 --decode)
+echo "${SERVICE_ACCOUNT_TOKEN_IN_K8S}" > sa.token
+printf "ca.crt and sa.token exported\\n"
